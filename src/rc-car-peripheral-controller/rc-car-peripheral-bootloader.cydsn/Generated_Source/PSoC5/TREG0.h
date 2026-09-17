6e7c1dbef94005d6871837a4ee177c34eb44d7ec
Carlos Duarte-Molina <carlosduarte.molina97@gmail.com>
2026-09-16 23:09:34 -0400

    feat: Added bootloader top design BSP

---
+++ src/rc-car-peripheral-controller/rc-car-peripheral-bootloader.cydsn/Generated_Source/PSoC5/TREG0.h
@@ -0,0 +1,83 @@
+/*******************************************************************************
+* File Name: TREG0.h  
+* Version 1.90
+*
+* Description:
+*  This file containts Status Register function prototypes and register defines
+*
+* Note:
+*
+********************************************************************************
+* Copyright 2008-2015, Cypress Semiconductor Corporation.  All rights reserved.
+* You may use this file only in accordance with the license, terms, conditions, 
+* disclaimers, and limitations in the end user license agreement accompanying 
+* the software package with which this file was provided.
+*******************************************************************************/
+
+#if !defined(CY_STATUS_REG_TREG0_H) /* CY_STATUS_REG_TREG0_H */
+#define CY_STATUS_REG_TREG0_H
+
+#include "cyfitter.h"
+
+#if ((CYDEV_CHIP_FAMILY_USED == CYDEV_CHIP_FAMILY_PSOC3) || \
+     (CYDEV_CHIP_FAMILY_USED == CYDEV_CHIP_FAMILY_PSOC4) || \
+     (CYDEV_CHIP_FAMILY_USED == CYDEV_CHIP_FAMILY_PSOC5))
+    #include "cytypes.h"
+    #include "CyLib.h"
+#else
+    #include "syslib/cy_syslib.h"
+#endif
+
+    
+/***************************************
+*     Data Struct Definitions
+***************************************/
+
+/* Sleep Mode API Support */
+typedef struct
+{
+    uint8 statusState;
+
+} TREG0_BACKUP_STRUCT;
+
+
+/***************************************
+*        Function Prototypes
+***************************************/
+
+uint8 TREG0_Read(void) ;
+void TREG0_InterruptEnable(void) ;
+void TREG0_InterruptDisable(void) ;
+void TREG0_WriteMask(uint8 mask) ;
+uint8 TREG0_ReadMask(void) ;
+
+
+/***************************************
+*           API Constants
+***************************************/
+
+#define TREG0_STATUS_INTR_ENBL    0x10u
+
+
+/***************************************
+*         Parameter Constants
+***************************************/
+
+/* Status Register Inputs */
+#define TREG0_INPUTS              8
+
+
+/***************************************
+*             Registers
+***************************************/
+
+/* Status Register */
+#define TREG0_Status             (* (reg8 *) TREG0_sts_sts_reg__STATUS_REG )
+#define TREG0_Status_PTR         (  (reg8 *) TREG0_sts_sts_reg__STATUS_REG )
+#define TREG0_Status_Mask        (* (reg8 *) TREG0_sts_sts_reg__MASK_REG )
+#define TREG0_Status_Aux_Ctrl    (* (reg8 *) TREG0_sts_sts_reg__STATUS_AUX_CTL_REG )
+
+#endif /* End CY_STATUS_REG_TREG0_H */
+
+
+/* [] END OF FILE */
---
+++ src/rc-car-peripheral-controller/rc-car-peripheral-bootloader.cydsn/codegentemp/rc-car-peripheral-bootloader_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\cayod\OneDrive\Documents\Repos\RC-Car-peripheral-controller\src\rc-car-peripheral-controller\rc-car-peripheral-bootloader.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\cayod\OneDrive\Documents\Repos\RC-Car-peripheral-controller\src\rc-car-peripheral-controller\rc-car-peripheral-bootloader.cydsn\rc-car-peripheral-bootloader.cyprj
+# Date: Thu, 17 Sep 2026 03:08:41 GMT
